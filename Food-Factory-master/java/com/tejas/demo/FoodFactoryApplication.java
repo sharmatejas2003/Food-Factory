41c552d182a8dedfb1eedc3286d849575fe904fb
Tejas Sharma <145262029+sharmatejas2003@users.noreply.github.com>
2026-01-16 21:39:02 +0530

    Update and rename Food-Factory-master/FoodFrenzyApplication.java to Food-Factory-master/java/com/tejas/demo/FoodFactoryApplication.java

--- Food-Factory-master/java/com/tejas/demo/FoodFactoryApplication.java
+++ Food-Factory-master/java/com/tejas/demo/FoodFactoryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class FoodFrenzyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodFrenzyApplication.class, args);
+		SpringApplication.run(FoodFactoryApplication.class, args);
 	}
 
 }
